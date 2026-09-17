5fa4e6027f361b9bfd4e60931c6bec5e49c08994
Романский Алексей <115107977+aromansky@users.noreply.github.com>
2026-09-17 14:59:32 +0300

    add lab impementation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-alert("Let's integrate!")
+function rightRiemannSum(f, a, b, n = 1000) {
+  const dx = (b - a) / n;
+  let sum = 0;
+
+  for (let i = 1; i <= n; i++) {
+    const x = a + i * dx;
+    sum += f(x);
+  }
+
+  return sum * dx;
+}
+
+const F = (x) => x ** 2 - x + 1;
+
+const inputA = prompt("Введите начальную точку интервала (a):");
+const inputB = prompt("Введите конечную точку интервала (b):");
+
+const a = parseFloat(inputA);
+const b = parseFloat(inputB);
+
+const result = rightRiemannSum(F, a, b);
+alert(`Результат интегрирования F(x) на отрезке [${a}, ${b}]: ${result}`);
